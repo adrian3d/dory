31c344b63f71fc255172ec3f5538d0f71b48fb5d
Adrian Vaucoret <adrian3ds@yahoo.fr>
2021-02-17 12:38:25 +0100

    Update target branche selector

--- dont-merge.js
+++ dont-merge.js
@@ -1,4 +1,4 @@
-let targetBranch = document.querySelector('#partial-discussion-header .TableObject .TableObject-item--primary .commit-ref').textContent;
+let targetBranch = document.querySelector('#partial-discussion-header .base-ref').textContent;
 let mergeButton = document.querySelector('.btn-group-merge');
 
 if (targetBranch !== 'master') {
