49c01ad009f7fce47ead0845abffa5e384fd4886
Adrian Vaucoret <adrian3ds@yahoo.fr>
2021-08-11 15:21:50 +0200

    Handle the change in the merge button rendering on github

--- dont-merge.js
+++ dont-merge.js
@@ -1,8 +1,30 @@
-let targetBranch = document.querySelector('#partial-discussion-header .base-ref').textContent;
-let mergeButton = document.querySelector('.btn-group-merge');
+console.info('Dory enabled');
 
-if (targetBranch !== 'master') {
-    mergeButton.style.backgroundColor = "purple";
-    mergeButton.style.color = "white";
-    mergeButton.style.backgroundImage = "none";
+function colorButton() {
+    const mergeButton = document.querySelector('.btn-group-merge');
+    const targetBranch = document.querySelector('#partial-discussion-header .base-ref').textContent;
+
+    console.info('[Dory] target branch: '  + targetBranch);
+
+    if (targetBranch !== 'master') {
+        mergeButton.style.backgroundColor = "purple";
+        mergeButton.style.color = "white";
+        mergeButton.style.backgroundImage = "none";
+    }
 }
+
+var observer = new MutationObserver(function (mutations, me) {
+    const mergeButton = document.querySelector('.btn-group-merge');
+
+    if (mergeButton) {
+        me.disconnect();
+        colorButton();
+
+        return;
+    }
+});
+
+observer.observe(document.getElementById('discussion_bucket'), {
+    childList: true,
+    subtree: true
+});
